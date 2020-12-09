453774365e79d5ab252b8ba032090f38d6f65168
Jasper Swallen <swallen@usc.edu>
2020-12-09 01:39:25 -0600

    try provided solution

--- main.cpp
+++ main.cpp
@@ -10,73 +10,33 @@
 
 UnbufferedSerial serial(USBTX, USBRX, BAUDRATE);
 SerialStream<UnbufferedSerial> pc(serial);
+char buf[CMD_BUFFER_SIZE];
+Thread t;
+EventQueue eventQueue;
 
-char cmdStr[CMD_BUFFER_SIZE];
-int currPos = 0;
-volatile bool pendingCmd = false;
-volatile bool charAvailable = false;
-
-void rxCallback(char c)
+void onSerialReceived(void)
 {
-    static char prev_char = '\0';
-    bool bufferFull = (currPos == (CMD_BUFFER_SIZE - 1));
+    char *p_buf = buf;
 
-    if(c == '\n' || c == '\r' || bufferFull)
+    memset(buf, 0, sizeof(buf));
+    while(pc.readable())
     {
-        if(prev_char == '\r')
-        {
-            prev_char = c;
-            return;
-        }
-
-        cmdStr[currPos] = '\0';
-        pendingCmd = true;
+        p_buf += pc.read(p_buf, sizeof(buf) - (p_buf - buf));
     }
-    else
+    if(p_buf > buf)
     {
-        pc.putc(c);
-        cmdStr[currPos] = c;
+        printf("Received: %s\r\n", buf);
     }
-    currPos++;
-    prev_char = c;
 }
 
-// A function that echoes any received data back
-void updateCommand(char *cmd)
+void onSigio(void)
 {
-    pc.printf("\r\nReceived CMD ");
-    pc.printf(cmd);
-    pc.printf("\r\n");
-
-    pendingCmd = false;
-    memset(cmdStr, 0, strlen(cmdStr));
-    currPos = 0;
+    eventQueue.call(onSerialReceived);
 }
 
-void charToRead()
+int main()
 {
-    charAvailable = true;
-}
-
-int main(void)
-{
-    pc.printf("Starting\r\n");
-    pc.attach(&charToRead, UnbufferedSerial::RxIrq);
-
-    while(1)
-    {
-        if(charAvailable)
-        {
-            char c;
-            pc.read(&c, 1);
-            rxCallback(c);
-            charAvailable = false;
-        }
-
-        if(pendingCmd)
-        {
-            updateCommand(cmdStr);
-        }
-        ThisThread::sleep_for(1s);
-    }
+    printf("Starting...\r\n");
+    t.start(callback(&eventQueue, &EventQueue::dispatch_forever));
+    pc.sigio(callback(onSigio));
 }
