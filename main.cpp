1d659f5967f90c4ff05ad2978c62a27011e6d306
Jasper Swallen <swallen@usc.edu>
2020-12-09 02:00:36 -0600

    try this

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@
 #define BAUDRATE 115200
 #define CMD_BUFFER_SIZE 32
 
-UnbufferedSerial serial(USBTX, USBRX, BAUDRATE);
-SerialStream<UnbufferedSerial> pc(serial);
-char buf[CMD_BUFFER_SIZE];
+BufferedSerial serial(USBTX, USBRX, 115200);
+char buf[64];
 Thread t;
 EventQueue eventQueue;
 
@@ -19,9 +18,9 @@ void onSerialReceived(void)
     char *p_buf = buf;
 
     memset(buf, 0, sizeof(buf));
-    while(pc.readable())
+    while(serial.readable())
     {
-        p_buf += pc.read(p_buf, sizeof(buf) - (p_buf - buf));
+        p_buf += serial.read(p_buf, sizeof(buf) - (p_buf - buf));
     }
     if(p_buf > buf)
     {
@@ -38,5 +37,8 @@ int main()
 {
     printf("Starting...\r\n");
     t.start(callback(&eventQueue, &EventQueue::dispatch_forever));
-    pc.sigio(callback(onSigio));
+    serial.sigio(callback(onSigio));
+    while(1)
+    {
+    }
 }
