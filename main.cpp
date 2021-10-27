9c23820ce79cbb630f0d43ae782b27bc5f4bd0e7
Jasper Swallen <swallen@usc.edu>
2021-10-26 23:20:22 -0700

    add temp sensor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <mbed.h>
 
 #include "KX134SPI.h"
+#include "LM75B.h"
 #include "ZEDFP9.h"
 
 constexpr PinName ACC_MOSI = PF_9;
@@ -15,12 +16,15 @@ constexpr PinName MISC_MOSI = PE_14;
 constexpr PinName MISC_MISO = PE_13;
 constexpr PinName MISC_SCLK = PE_12;
 
+constexpr PinName I2C3_SDA = PC_9;
+constexpr PinName I2C3_SCL = PA_8;
+
 void connect_to_accel()
 {
 
     KX134SPI accel(ACC_MOSI, ACC_MISO, ACC_SCLK, ACC_CS);
 
-    printf("Connecting to accelerometer...\r\n");
+    printf("\r\n\r\nConnecting to accelerometer...\r\n");
     if (accel.init())
     {
         printf("Successfully Connected!\r\n");
@@ -44,10 +48,29 @@ void connect_to_gps()
 {
     UBlox::ZEDF9P gps(MISC_MOSI, MISC_MISO, GPS_RST, MISC_SCLK, GPS_CS);
 
-    printf("Connecting to GPS...\r\n");
+    printf("\r\n\r\nConnecting to GPS...\r\n");
     if (gps.begin(true))
     {
         printf("Successfully Connected!\r\n");
+    }
+    else
+    {
+        printf("Failed to connect\r\n");
+    }
+}
+
+void connect_to_temp_sensor()
+{
+    LM75B temp(I2C3_SDA, I2C3_SCL);
+
+    printf("\r\n\r\nConnecting to temp sensor...\r\n");
+    if (temp.open())
+    {
+        printf("Successfully Connected!\r\n");
+
+        ThisThread::sleep_for(1s);
+
+        printf("Read %fC temperature\r\n", temp.temp());
     }
     else
     {
